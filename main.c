e439002e7f76e3ed59914b5346f8ead54014cd49
Lena Vogelsang <lenav@Lenas-MacBook-Pro.local>
2022-10-17 13:45:57 +1030

    l

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int	main(void)
 {
+/*	int	i = printf("%c", 0);
+	int	j = ft_printf("%c", 0);
+	printf("%d\n%d\n", i, j);
 	printf("%-2c", '0');
 	printf("%-3c", '0' - 256);
 	printf("%-4c", '0' + 256);
@@ -20,4 +23,8 @@ int	main(void)
 	ft_printf("%-1c %-2c %-3c", '1', '2', '3');
 	ft_printf("%-1c %-2c %-3c", '2', '1', 0);
 	ft_printf("%-1c %-2c %-3c\n", 0, '1', '2');
+*/	printf(" %-1c %-2c %-3c \n", '0', 0, '1');
+	ft_printf(" %-1c %-2c %-3c \n", '0', 0, '1');
+	printf(" %-1c %-2c %-3c \n", '2', '1', 0);
+	ft_printf(" %-1c %-2c %-3c \n", '2', '1', 0);
 }
